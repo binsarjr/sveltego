e1c89cf6787865dabea69bdb837e974d75a75238
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2026-04-30 22:30:33 +0700

    fix(sveltego): use slog.Uint64 for uint64 stream_id slog attribute

--- packages/sveltego/server/pipeline.go
+++ packages/sveltego/server/pipeline.go
@@ -605,14 +605,14 @@ func (s *Server) renderStreaming(w http.ResponseWriter, r *http.Request, ev *kit
 		if ctx.Err() != nil {
 			s.cancelStreams(streams)
 			s.Logger.DebugContext(ctx, "streaming: client disconnected mid-stream",
-				slog.String("stream_id", f.id))
+				slog.Uint64("stream_id", f.id))
 			return kit.ErrClientGone
 		}
 		if err := buf.FlushTo(w); err != nil {
 			if isClientGone(ctx, err) {
 				s.cancelStreams(streams)
 				s.Logger.DebugContext(ctx, "streaming: client disconnected mid-stream (write)",
-					slog.String("stream_id", f.id))
+					slog.Uint64("stream_id", f.id))
 				return kit.ErrClientGone
 			}
 			return err
